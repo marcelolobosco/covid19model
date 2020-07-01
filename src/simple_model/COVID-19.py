b77e7e272e02abe79841a8f84a7dc1fd863a2402
Lara Pompei <pompei@pop-os.localdomain>
2020-07-01 19:24:31 -0300

    inclusão da Citocina no modelo

--- src/simple_model/COVID-19.py
+++ src/simple_model/COVID-19.py
@@ -5,15 +5,18 @@ Created on Thu Apr 25 10:06:01 2019
 
 @author: carlabonin
 """
+#Quando executar esse código, passar por parâmetro o "n" para simular os non-survivors
+
 import numpy as np
 from scipy import integrate
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-from Modelo import *
+from ModeloCitocina import *
 from scipy.stats.mstats import gmean
 import pylab
 import math
+import sys
 sns.set_style("whitegrid")
 
 #######  CONDIÇÕES INICIAIS PARA AJUSTE DE UNIDADES
@@ -32,16 +35,46 @@ Pl0 = 0.0
 Bm0 = 0.0
 A0_M = 0.0  
 A0_G = 0.0
+C0 = 0.0
 
-model_args = (1.32, 2.63, 0.60, 0.000120532191*0.4, 1.87E-06*0.4, 2.50E-03, 5.5e-01, 0.8, 40.0,      5.38E-01, 2.17E-04, 1.0E-05, 1.0E-08,  0.0003,   6.0E+00, 4.826E-06, 1.27E-8, 0.000672, 5.61E-06, 1.0E-06,   2.0, 2.3976E-04, 9.75E-04, 1.0e-5, 2500.0, 0.002,0.00068,     0.01,       4.826E-06, 2.17E-04, 1.0E-07, 1.0E-08, 0.22, 1.0e6, 1.0e6, 5.0e5, 2.5E5)#,2.63, 0.60, 9.80167723e-02, 6.40994768e-05, 1.87E-06*0.4, 2.50E-03, 5.5e-01, 0.8, 40.0, 8.14910996e+00, 2.17E-04, 1.0E-05, 1.0E-08, 0.1*0.003, 6.55248840e+01, 4.826E-06, 1.27E-10*100.0, 0.000672, 5.61E-06, 1.0E-06, 2.0, (2.22E-04)*1.8*0.6, (1.95E-06)*500.0, 1.0e-5, 2500.0, 0.002, 0.00068, 8.00694162e-02, 3.06889922e-01, 2.17E-04, 1.0E-07, 1.0E-08, 0.22)
+#            (pi_v, c_v1, c_v2, k_v1, k_v2, alpha_Ap, beta_Ap, k_ap1, k_ap2, delta_Apm, alpha_Tn, pi_T, k_te1, delta_te, alpha_B, pi_B1, pi_B2, beta_S, beta_L, beta_Bm, delta_S, delta_L, gamma_M, k_bm1, k_bm2, pi_AS, pi_AL, delta_A_G, delta_A_M, c11, c12, c13, c14, pi_capm, pi_cthe, pi_ctke, pi_cb, delta_c, Ap0, Thn0, Tkn0, B0)
+
+#Modelo sem citocina, argumentos:
+#model_args = (1.32, 2.63, 0.60, 0.000120532191*0.4, 1.87E-06*0.4, 2.50E-03, 5.5e-01, 0.8, 40.0,      5.38E-01, 2.17E-04, 1.0E-05, 1.0E-08,  0.0003,   6.0E+00, 4.826E-06, 1.27E-8, 0.000672, 5.61E-06, 1.0E-06,   2.0, 2.3976E-04, 9.75E-04, 1.0e-5, 2500.0, 0.002,0.00068,     0.01,       4.826E-06, 2.17E-04, 1.0E-07, 1.0E-08, 0.22, 1.0e6, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0e6, 5.0e5, 2.5E5)#,2.63, 0.60, 9.80167723e-02, 6.40994768e-05, 1.87E-06*0.4, 2.50E-03, 5.5e-01, 0.8, 40.0, 8.14910996e+00, 2.17E-04, 1.0E-05, 1.0E-08, 0.1*0.003, 6.55248840e+01, 4.826E-06, 1.27E-10*100.0, 0.000672, 5.61E-06, 1.0E-06, 2.0, (2.22E-04)*1.8*0.6, (1.95E-06)*500.0, 1.0e-5, 2500.0, 0.002, 0.00068, 8.00694162e-02, 3.06889922e-01, 2.17E-04, 1.0E-07, 1.0E-08, 0.22)
+
+#                (pi_v, c_v1, c_v2, k_v1              , k_v2        , alpha_Ap, beta_Ap, k_ap1, k_ap2, delta_Apm, alpha_Tn,  pi_T  , k_te1  , delta_te, alpha_B,   pi_B1  , pi_B2  , beta_S  , beta_L  , beta_Bm, delta_S,  delta_L  , gamma_M , k_bm1 , k_bm2 , pi_AS, pi_AL , delta_A_G, delta_A_M,   c11   ,  c12   ,   c13  , c14 , pi_capm, pi_cthe, pi_ctke, pi_cb, delta_c, Ap0  , Thn0 , Tkn0 , B0)
+#modelo com citocina-naoSobrevivente, argumentos:
+if sys.argv[-1] == 'n':
+    model_args = (1.32, 2.63, 0.60, 0.000120532191*0.4, 1.87E-06*0.4, 2.50E-03, 5.5e-01, 0.8  , 40.0 , 5.38E-01 , 2.17E-04, 1.0E-05, 1.0E-08, 0.0003  , 6.0E+00, 4.826E-06, 1.27E-8, 0.000672, 5.61E-06, 1.0E-06,   2.0  , 2.3976E-04, 9.75E-04, 1.0e-5, 2500.0, 0.002,0.00068,   0.01   , 4.826E-06, 2.17E-04, 1.0E-07, 1.0E-08, 0.22, 8.0e-6 , 1.0e-5 , 1.0e-6 ,  3.0e-6 , 0.2    , 1.0e6, 1.0e6, 5.0e5, 2.5E5)
+#experimentando
+#pi_camp: muito efeito, range:(1.0e-6 - 5.0e-6) - do dia 0 ao 11(pico:11)
+#pi_cthe: range(8.0e-6, 1.0e-5) - do dia 0-20 (pico: 20)
+#pi_ctke: range(7.0e-7, 1.0e-6) - pico: o crescimento nao tem pico e a inclinaćão aumenta
+#pi_cb: range(1.0e-6, 9.0e-6) - pico:
+#delta_c: range(0.1-1.0)
+
+#                (pi_v, c_v1, c_v2, k_v1              , k_v2        , alpha_Ap, beta_Ap, k_ap1, k_ap2, delta_Apm, alpha_Tn,  pi_T  , k_te1  , delta_te, alpha_B,   pi_B1  , pi_B2  , beta_S  , beta_L  , beta_Bm, delta_S,  delta_L  , gamma_M , k_bm1 , k_bm2 , pi_AS, pi_AL , delta_A_G, delta_A_M,   c11   ,  c12   ,   c13  , c14 , pi_capm, pi_cthe, pi_ctke, pi_cb, delta_c, Ap0  , Thn0 , Tkn0 , B0)
+#modelo com citocina-sobrevivente, argumentos:
+else:
+    model_args = (1.32, 2.63, 0.60, 0.000120532191*0.4, 1.87E-06*0.4, 2.50E-03, 5.5e-01, 0.8  , 40.0 , 5.38E-01 , 2.17E-04, 1.0E-05, 1.0E-08, 0.0003  , 6.0E+00, 4.826E-06, 1.27E-8, 0.000672, 5.61E-06, 1.0E-06,   2.0  , 2.3976E-04, 9.75E-04, 1.0e-5, 2500.0, 0.002,0.00068,   0.01   , 4.826E-06, 2.17E-04, 1.0E-07, 1.0E-08, 0.22, 5.0e-6 , 1.0e-5 , 1.0e-6 ,  3.5e-6 , 0.3    , 1.0e6, 1.0e6, 5.0e5, 2.5E5)
+#experimentando
+#pi_camp: muito efeito, range:(1.0e-6 - 5.0e-6) - do dia 0 ao 11(pico:11)
+#pi_cthe: range(8.0e-6, 1.0e-5) - do dia 0-20 (pico: 20)
+#pi_ctke: range(7.0e-7, 1.0e-6) - pico: o crescimento nao tem pico e a inclinaćão aumenta
+#pi_cb: range(1.0e-6, 9.0e-6) - pico:
+#delta_c: range(0.1-1.0)
 
 t=np.linspace(0,45,180)    
-y,d=integrate.odeint(immune_response, [V0,Ap0,Apm0,Thn0,The0,Tkn0,Tke0,B0,Ps0,Pl0,Bm0,A0_M,A0_G], t, args=(model_args), full_output=1, printmessg=True)
+y,d=integrate.odeint(immune_response, [V0,Ap0,Apm0,Thn0,The0,Tkn0,Tke0,B0,Ps0,Pl0,Bm0,A0_M,A0_G, C0], t, args=(model_args), full_output=1, printmessg=True)
 
 
 #######   Viremia  log10 copias/ml ##########
 dadosViremiaLog10 = pd.read_csv('../../data/Viral_load_10_2.csv',',')
 dadosAnticorposLog2 = pd.read_csv('../../data/IgG_IgM_21_1b.csv',',') 
+if sys.argv[-1] == 'n':
+    dadosCitocina = pd.read_csv('../../data/IL6_non-survivors_19.csv',',')
+else:
+    dadosCitocina = pd.read_csv('../../data/IL6_survivors_19.csv',',')
 
 media_igG =[]
 media_igM = []
@@ -90,5 +123,12 @@ plt.xlabel('Tempo pós-vacinação (dias)')
 plt.ylabel('Viremia')
 plt.legend()
 #plt.savefig('FigurasTese/Primovacinados/AnticorposPrimovacinadosCurvaAjuste2.pdf',bbox_inches='tight',dpi = 300)
+
+dadosCitocina.plot.scatter(x='Day',y='IL6(pg/mL)',color='y',label='Dados experimentais')
+plt.xlim(0.0,45.0)
+plt.plot(t,y[:,13],label='Curva gerada pelo modelo',linewidth=1.5, linestyle="-")
+plt.xlabel('Tempo pós-infecção (dias)')
+plt.ylabel('Cytokine')
+plt.legend()
 
 plt.show() 
